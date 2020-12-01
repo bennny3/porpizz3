b673397f54961b02c8a9836e96746be47059656c
bennny3 <75240393+bennny3@users.noreply.github.com>
2020-12-01 12:48:58 +0700

    porpizz websites

---
+++ PORPIZ/main.js
@@ -0,0 +1,12 @@
+// const menuToggle = document.querySelector('.menu-toggle');
+// let menuOpen = false;
+// menuToggle.addEventListener('click',() => {
+//     if(!menuToggle){
+//         menuToggle.classList.add('open');
+//         menuOpen = true;
+//     }
+//     else{
+//         menuToggle.classList.remove('open');
+//         menuOpen = false;
+//     }
+// });
